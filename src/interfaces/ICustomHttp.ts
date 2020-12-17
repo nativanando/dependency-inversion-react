847abf21a8bfc4a40d37be0b5877db4ce69b81ee
Fernando Natividade <nativanando@gmail.com>
2020-12-17 08:41:26 -0300

    modifying ICustomHttp interface

--- src/interfaces/ICustomHttp.ts
+++ src/interfaces/ICustomHttp.ts
@@ -2,23 +2,17 @@ import HttpCustomRequest from '../classes/CustomHttpRequest';
 import HttpCustomResponse from '../classes/CustomHttpResponse';
 
 interface ICustomHttp {
-  get<T extends unknown>(
+  get<T>(httpCustomRequest: HttpCustomRequest<T>): Promise<HttpCustomResponse>;
+
+  post<T>(httpCustomRequest: HttpCustomRequest<T>): Promise<HttpCustomResponse>;
+
+  put<T>(httpCustomRequest: HttpCustomRequest<T>): Promise<HttpCustomResponse>;
+
+  patch<T>(
     httpCustomRequest: HttpCustomRequest<T>,
   ): Promise<HttpCustomResponse>;
 
-  post<T extends unknown>(
-    httpCustomRequest: HttpCustomRequest<T>,
-  ): Promise<HttpCustomResponse>;
-
-  put<T extends unknown>(
-    httpCustomRequest: HttpCustomRequest<T>,
-  ): Promise<HttpCustomResponse>;
-
-  patch<T extends unknown>(
-    httpCustomRequest: HttpCustomRequest<T>,
-  ): Promise<HttpCustomResponse>;
-
-  delete<T extends unknown>(
+  delete<T>(
     httpCustomRequest: HttpCustomRequest<T>,
   ): Promise<HttpCustomResponse>;
 }
